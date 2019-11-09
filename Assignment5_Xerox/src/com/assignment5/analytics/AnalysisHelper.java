2e3de3c9e4603c3556842e62cdf43ae42bda209a
Yu-Ting Tsai <tsai.yut>
2019-11-08 23:23:31 -0500

    code enhance

--- Assignment5_Xerox/src/com/assignment5/analytics/AnalysisHelper.java
+++ Assignment5_Xerox/src/com/assignment5/analytics/AnalysisHelper.java
@@ -9,8 +9,8 @@ import com.assignment5.entities.Item;
 import com.assignment5.entities.Order;
 import com.assignment5.entities.Product;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -22,12 +22,17 @@ public class AnalysisHelper {
     //find top 3 best prodcuts
     public void topThreeProducts() {
         Map<Integer, Item> items = DataStore.getInstance().getItems();
-        List<Integer> idList = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
+        Map<Integer, Integer> productIdMap = new HashMap<>();
         for(Item item: items.values()) {
             int productId = item.getProductId();
-            int i = idList.get(productId) + item.getQuantity();
-            idList.set(productId, i);
+            if (productIdMap.containsKey(productId)) {
+                int i = productIdMap.get(productId) + item.getQuantity();
+                productIdMap.put(productId, i);
+            } else {
+                productIdMap.put(productId, item.getQuantity());
+            }
         }
+        List<Integer> idList = new ArrayList<>(productIdMap.values());
         List<Integer> idListStore = new ArrayList<>(idList);
         Collections.sort(idList);
         Collections.reverse(idList);
@@ -43,29 +48,38 @@ public class AnalysisHelper {
         Map<Integer, Order> orders = DataStore.getInstance().getOrders();
         Map<Integer, Product> products = DataStore.getInstance().getProducts();
         List<Product> productList = new ArrayList<>(products.values());
-        List<Integer> idList = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
-        List<Integer> salesList = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
+        Map<Integer, Integer> customerIdMap = new HashMap<>();
+        Map<Integer, Integer> salesIdMap = new HashMap<>();
         for(Order order: orders.values()) {
             int customerId = order.getCustomerId();
             int salesId = order.getSupplierId();
             int productId = order.getItem().getProductId();
-            int salesPrice = order.getItem().getSalesPrice();
-            int targetPrice = productList.get(productId).getTargetPrice();
+            int priceDifference = order.getItem().getSalesPrice() - productList.get(productId).getTargetPrice();
             int quantity = order.getItem().getQuantity();
-            int i = idList.get(customerId) + salesPrice - targetPrice;
-            idList.set(customerId, i);
-            int j = salesList.get(salesId) + (salesPrice - targetPrice) * quantity;
-            salesList.set(salesId, j);
+            if (customerIdMap.containsKey(customerId)) {
+                int i = customerIdMap.get(customerId) + priceDifference;
+                customerIdMap.put(customerId, i);
+            } else {
+                customerIdMap.put(customerId, priceDifference);
+            }
+            if (salesIdMap.containsKey(salesId)) {
+                int i = salesIdMap.get(salesId) + priceDifference * quantity;
+                salesIdMap.put(salesId, i);
+            } else {
+                salesIdMap.put(salesId, priceDifference * quantity);
+            }
         }
-        List<Integer> idListStore = new ArrayList<>(idList);
-        Collections.sort(idList);
-        Collections.reverse(idList);
+        List<Integer> customerList = new ArrayList<>(customerIdMap.values());
+        List<Integer> customerListStore = new ArrayList<>(customerList);
+        Collections.sort(customerList);
+        Collections.reverse(customerList);
         System.out.println("\nTop 3 best customers: ");
         for (int i = 0; i < 3; i++) {
-            int j = idList.get(i);
-            System.out.println("customer id: " + idListStore.indexOf(j) + ", sum of difference: " + j);
-            idListStore.set(idListStore.indexOf(j), -1);
+            int j = customerList.get(i);
+            System.out.println("customer id: " + customerListStore.indexOf(j) + ", sum of difference: " + j);
+            customerListStore.set(customerListStore.indexOf(j), -1);
         }
+        List<Integer> salesList = new ArrayList<>(salesIdMap.values());
         List<Integer> salesListStore = new ArrayList<>(salesList);
         Collections.sort(salesList);
         Collections.reverse(salesList);
